8bfd387a1c4aad5dd7df1b6d7ff1e13276e5fed0
KayqueFerreira <60491908+KayqueFerreira@users.noreply.github.com>
2020-01-31 08:42:45 -0300

    Initial commit

---
+++ conexao.php
@@ -0,0 +1,12 @@
+﻿<?php
+$HOST  =  "localhost";
+$USER  = "root";
+$PASS  = "";
+$PORT ="3308";
+$BANCO =  "projeto_teste";
+
+$concta = mysqli_connect($HOST,$USER,$PASS,$BANCO,$PORT);
+$db = mysqli_select_db($concta, $BANCO );
+
+
+?>
